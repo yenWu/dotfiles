461722a88aefe9c998ed20b9426f41f5a2a78006
yenWu <yenwu.tw@gmail.com>
2019-09-01 18:53:06 +0800

    script: package installation

---
+++ pack-install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Env
+DISTRO="$(lsb_release -si)"
+RELEASE="$(lsb_release -sr)"
+
+# Flag
+## Only test on Ubuntu 18.04
+## PK   = package
+## MAN  = manager 
+SUDO="sudo"
+PKMAN="apt-get"
+PKINSTALL="$PKMAN install -y"
+
+PKS="neovim fish"
+
+# Installation
+$SUDO $PKINSTALL $PKS
